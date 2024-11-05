a7d49b8f1ec97b9a10e8a032b0525e25331f6f5a
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2024-11-06 01:11:51 +0700

    initial commit from Spring Initializr

---
+++ src/main/java/com/example/omaapinions/OmaapinionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.omaapinions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmaapinionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmaapinionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/omaapinions/OmaapinionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omaapinions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmaapinionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
