a32a251f43549d4da92411c0b478af5be34c9be5
monica <monicadestianti@gmail.com>
2024-12-12 23:56:21 +0700

    add submission model

---
+++ src/main/java/com/example/omaapinions/models/Submission.java
@@ -0,0 +1,38 @@
+package com.example.omaapinions.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "submissions")
+public class Submission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserSurvey user;
+    @ManyToOne
+    @JoinColumn(name = "survey_id", nullable = false)
+    private Survey survey;
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+    @Column
+    private String answer;
+}
