349762443a4c2c93a3b53deb4757c82a655406c2
rrxfanda <daffaadjierefanda@gmail.com>
2024-12-14 00:10:09 +0700

    Menambahkan Models User Survey

---
+++ src/main/java/com/example/omaapinions/models/UserSurvey.java
@@ -0,0 +1,40 @@
+package com.example.omaapinions.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "users")
+public class UserSurvey {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<RoleEnum> roles = new HashSet<>();
+
+    public enum RoleEnum {
+        ADMIN,
+        USER
+    }
+}
