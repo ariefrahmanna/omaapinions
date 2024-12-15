5d9818027be1fd5f3a8084fa159b4fc656bd0119
alexandersrtn <alexander.dprs@gmail.com>
2024-12-15 18:14:19 +0700

    add survey mapper

---
+++ src/main/java/com/example/omaapinions/mapper/SurveyMapper.java
@@ -0,0 +1,30 @@
+package com.example.omaapinions.mapper;
+
+import java.util.stream.Collectors;
+
+import com.example.omaapinions.dto.SurveyDto;
+import static com.example.omaapinions.mapper.QuestionMapper.mapToQuestionDto;
+import com.example.omaapinions.models.Survey;   
+
+public class SurveyMapper {
+
+    public static SurveyDto mapToSurveyDto(Survey survey) {
+        return SurveyDto.builder()
+                .id(survey.getId())
+                .title(survey.getTitle())
+                .category(survey.getCategory())
+                .duration(survey.getDuration())
+                .questions(survey.getQuestions().stream().map((question) -> mapToQuestionDto(question))
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+    public static Survey mapToSurvey(SurveyDto survey) {
+        return Survey.builder()
+                .id(survey.getId())
+                .title(survey.getTitle())
+                .category(survey.getCategory())
+                .duration(survey.getDuration())
+                .build();
+    }
+}
