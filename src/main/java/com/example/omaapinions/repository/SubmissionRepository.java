3cf290599ca96364e4414693a42ba23ba8c9bdbf
karuqii9704 <rifqisigwannugraha@gmail.com>
2024-12-18 14:04:47 +0700

    add question repository

---
+++ src/main/java/com/example/omaapinions/repository/SubmissionRepository.java
@@ -0,0 +1,11 @@
+package com.example.omaapinions.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.omaapinions.models.Submission;
+import com.example.omaapinions.models.UserSurvey;
+
+public interface SubmissionRepository extends JpaRepository<Submission, Long> {
+
+    boolean existsBySurveyIdAndUser(Long surveyId, UserSurvey user);
+}
